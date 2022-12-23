401f083961cfd2be286ec8d76a93f4a9b8a39c91
Sanjay <sanjaykumarme40@gmail.com>
2022-12-23 10:11:49 +0530

    rsc-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import CompoMessa from "./Components/CompoMessa";
+import CompoMessa10 from "./Components/CompoMessa10";
+import CompoMessa2 from "./Components/CompoMessa2";
+import CompoMessa5 from "./Components/CompoMessa5";
+import CompoMessa6 from "./Components/CompoMessa6";
+import CompoMessa7 from "./Components/CompoMessa7";
+import CompoMessa8 from "./Components/CompoMessa8";
+import CompoMessa9 from "./Components/CompoMessa9";
+import CompoMessage from './Components/CompoMessage';
+import Navbar from "./Components/Navbar";
+// import Popup from "./Components/Popup";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    {/* <Popup/> */}
+    <Navbar/>
+      <CompoMessage/>
+      <CompoMessa/>
+      <CompoMessa2/>
+      <CompoMessa5/>
+      <CompoMessa6/>
+      <CompoMessa7/>
+      <CompoMessa8/>
+      <CompoMessa9/>
+      <CompoMessa10/>
     </div>
   );
 }
